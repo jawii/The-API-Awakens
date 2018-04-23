eeb19ca8b0280328c74532e1087c2d8d54a2df86
Jaakko Kenttä <kenttajaa@gmail.com>
2018-04-23 09:46:11 +0300

    Initial Commit

---
+++ The API AwakensTests/The_API_AwakensTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_API_AwakensTests.swift
+//  The API AwakensTests
+//
+//  Created by Jaakko Kenttä on 23/04/2018.
+//  Copyright © 2018 Jaakko Kenttä. All rights reserved.
+//
+
+import XCTest
+@testable import The_API_Awakens
+
+class The_API_AwakensTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
